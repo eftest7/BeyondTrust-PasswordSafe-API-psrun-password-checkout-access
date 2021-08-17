e0fc246b367f4e6899baa9f03958207b45e757eb
danz777 <40545998+danz777@users.noreply.github.com>
2021-08-17 07:30:24 -0700

    Add files via upload

---
+++ powershell-pws-psrun-api.ps1
@@ -0,0 +1,11 @@
+# BeyondInsight/PasswordSafe Variable Information
+$binsight = 'BeyondInsight hostname here'
+$apikey = 'API key kere'
+$api_user = 'API user name here'
+$reason = 'API Test'
+$systemtofind = 'PM4W-Demo-02'
+$managed_user = 'stan-adm'
+
+#<----RETRIEVE A PASSWORD---->
+$pw_result = C:\code\psrun\psrun2.exe -i $binsight $apikey $api_user RetrievePassword $systemtofind $managed_user $reason
+write-output 'Password for' $managed_user 'on' $systemtofind 'is:' $pw_result
